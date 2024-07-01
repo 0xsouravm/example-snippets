b4a2d2e92dc8b68e5923b54d0244309013a18d61
Sourav Mishra <sourav.m.bt@gmail.com>
2024-07-01 19:46:24 +0530

    guard and | explaination

--- e_match/src/main.rs
+++ e_match/src/main.rs
@@ -1,13 +1,14 @@
-// Match is a control flow statement that allows us to compare a value against 
+// Match is a control flow statement that allows us to compare a value against
 // a series of patterns and then execute code based on which pattern matches.
 
 // Exhaustiveness
 
 fn main() {
     // basic_match();
-    match_range();
+    // match_range();
+    // match_with_guard();
+    match_multiple();
 }
-
 
 fn basic_match() {
     let a = 100;
@@ -57,7 +58,6 @@ fn match_tuple() {
         (_, _, 3, ..) => println!("Third element is 3"),
         (.., 4) => println!("Fourth element is 4"),
         // (_, _, _, 4) => println!("Fourth element is 4"),
-
         (x, y, z, a) => println!("{}, {}, {}, {}", x, y, z, a),
         // (1, _, _, _) => println!("First element is 1"),
         // (_, 2, _, _) => println!("Second element is 2"),
@@ -72,23 +72,43 @@ fn match_with_code_block() {
             println!("a is 100");
             println!("a is 100");
             println!("a is 100");
-        },
+        }
 
         _ => println!("a is something else"),
     }
 }
 
+// match with guard
 
+// Guard - Additional condition that allows for extra filteration
+fn match_with_guard() {
+    let a = 200;
+    // let b = 1..=100;
+    // let b = 1..100;
+    match a {
+        0..=200 if a % 2 == 0 => {
+            println!("a is between 0 and 200 and is even");
+        },
+        _ => println!("a is something else"),
+    }
+}
 
+// a = 3 | 8 | 203;
 
+// a % 2 == 0/1
 
-
+fn match_multiple() {
+    // let a = 3;
+    // let a = 8;
+    let a = 1234567;
+    match a {
+        3 | 8 | 203 => {println!("a is 3, 8, or 203")},
+        _ => println!("a is something else"),
+    }
+}
 
 // (1, 2, 4, 5, 6, 7, 8, 9, 10);
 // (1, 2, 4, 5, 1, 2, 3, 4, 6, 7, 8, 9, 10);
 // (1, 2, 6, 10);
 
-
 // (..)
-
-
